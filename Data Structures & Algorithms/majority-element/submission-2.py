c9a5ba5131806708b2f2a75b6a328546790286cb
Bhuvan Vadlapudi <124895531+BhuvanVadlapudi@users.noreply.github.com>
2026-08-12 15:04:51 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        cond = n//2 
+        max_val = min(nums)
+        unique_nums = list(set(nums))
+        for i in unique_nums:
+            count_n = nums.count(i)
+            if(count_n > cond):
+                max_value = max(max_val,i)
+        return max_value
+        
