707ecf5237e30965d61f04528c56a947631538a2
Bhuvan Vadlapudi <124895531+BhuvanVadlapudi@users.noreply.github.com>
2026-09-25 14:59:41 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,51 @@
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+        postfix = 1
+        res = [None]*len(nums)
+
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i] 
+
+
+        for i in range(len(nums)-1,-1,-1):
+            res[i] = res[i]*postfix
+            postfix *= nums[i]
+
+
+        return res
+
+        # res = []
+        # count_zeros = nums.count(0)
+        # print(count_zeros)
+        # if(count_zeros > 1):
+        #     return [0]*len(nums)
+        # elif(count_zeros == 1):
+        #     zeroth_index = nums.index(0)
+        #     val = 1
+        #     for i in nums:
+        #         if(i != 0):
+        #             val *= i
+        #     res = [0]*len(nums)
+        #     res[zeroth_index] = val
+        #     return res
+        # else:
+        #     total_product = 1
+        #     res = [None]*len(nums)
+        #     for i in nums:
+        #         total_product *= i
+        #     for j in range(len(nums)):
+        #         res[j] = total_product//nums[j]
+        #     return res
+
+        #Brute-Force Approach
+        # res = []
+        # for i in range(len(nums)):
+        #     val = 1
+        #     for j in range(len(nums)):
+        #         if(i != j):
+        #             val *= nums[j]
+        #     res += [val]
+        # return res
